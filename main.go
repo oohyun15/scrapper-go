94ddb1aa33318d27b079c047154626468d9b8667
oohyun15 <sakiss@ajou.ac.kr>
2020-09-02 23:08:23 +0900

    set scrapper func

--- main.go
+++ main.go
@@ -1,8 +1,27 @@
 package main
 
-import "github.com/oohyun15/learngo/scrapper"
+import (
+	"strings"
+
+	"github.com/oohyun15/scrapper-go/scrapper"
+
+	"github.com/labstack/echo"
+)
+
+const fileName string = "jobs.csv"
+
+func handleHome(c echo.Context) error {
+	return c.File("home.html")
+}
+
+func handleScrape(c echo.Context) error {
+	term := strings.ToLower(scrapper.CleanString(c.FormValue("term")))
+	return nil
+}
 
 func main() {
-
-	scrapper.Scrape("term")
+	e := echo.New()
+	e.GET("/", handleHome)
+	e.POST("/scrape", handleScrape)
+	e.Logger.Fatal(e.Start(":3000"))
 }
